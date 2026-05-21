b176466b1d3896ab36a2e24cc35eec6823ff35f3
Rudhar <rudharbajaj@gmail.com>
2026-05-21 18:29:11 +0530

    added PBR

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
 
 function Ball() {
+  const ref = useRef()
+
+  useFrame((_, delta) => {
+    ref.current.rotation.y += delta * 0.5
+    ref.current.rotation.x += delta * 0.1
+  })
+
   return (
-    <mesh position={[0, 0, 0]}>
+    <mesh ref={ref} position={[0, 0, 0]}>
       <sphereGeometry args={[1, 64, 64]} />
-      <meshBasicMaterial color="white" />
+      <meshStandardMaterial color="#ffffff" roughness={0.4} metalness={0.0} />
     </mesh>
   )
 }
@@ -12,6 +20,8 @@ function Ball() {
 export default function App() {
   return (
     <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
+      <ambientLight intensity={0.3} />
+      <directionalLight position={[3, 3, 5]} intensity={2.5} />
       <Ball />
     </Canvas>
   )
