6d41904f70d5b5ec78234fedcca94f298fd66843
Rudhar <rudharbajaj@gmail.com>
2026-05-21 18:37:03 +0530

    added rapier physics of ball dropping

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
-import { Canvas, useFrame } from '@react-three/fiber'
-import { useRef } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Physics, RigidBody } from '@react-three/rapier'
 
 function Ball() {
-  const ref = useRef()
-
-  useFrame((_, delta) => {
-    ref.current.rotation.y += delta * 0.5
-    ref.current.rotation.x += delta * 0.1
-  })
-
   return (
-    <mesh ref={ref} position={[0, 0, 0]}>
-      <sphereGeometry args={[1, 64, 64]} />
-      <meshStandardMaterial color="#ffffff" roughness={0.4} metalness={0.0} />
-    </mesh>
+    <RigidBody colliders="ball" restitution={0.88} position={[0, 4, 0]}>
+      <mesh>
+        <sphereGeometry args={[0.4, 64, 64]} />
+        <meshStandardMaterial color="#ffffff" roughness={0.4} metalness={0.0} />
+      </mesh>
+    </RigidBody>
+  )
+}
+
+function Floor() {
+  return (
+    <RigidBody type="fixed" colliders="cuboid" position={[0, -1.5, 0]}>
+      <mesh>
+        <boxGeometry args={[10, 0.2, 10]} />
+        <meshStandardMaterial color="#0a3a73" roughness={0.3} metalness={0.0} />
+      </mesh>
+    </RigidBody>
   )
 }
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
+    <Canvas camera={{ position: [0, 1, 5], fov: 50 }}>
       <ambientLight intensity={0.3} />
-      <directionalLight position={[3, 3, 5]} intensity={2.5} />
-      <Ball />
+      <directionalLight position={[3, 5, 5]} intensity={2.5} castShadow />
+      <Physics gravity={[0, -9.81, 0]}>
+        <Ball />
+        <Floor />
+      </Physics>
     </Canvas>
   )
 }
